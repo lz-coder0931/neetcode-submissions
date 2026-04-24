689419f0cac0cc5324efa24eaa9a6ef2ca3c7fce
lz-coder0931 <18993293277.lz@gmail.com>
2026-04-24 10:44:39 -0700

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # brute force
+        res = 0
+        for i in range(len(prices)-1):
+            for j in range(i+1,len(prices)):
+                res = max(prices[j]-prices[i],res)
+
+        return res
+        
